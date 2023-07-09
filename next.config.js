497964673a7398c54c5c40d378216ef5312294bd
Taras Stukalo <taras.frbslist@gmail.com>
2023-07-09 13:36:12 +0300

    add env siteUrl

--- next.config.js
+++ next.config.js
@@ -2,10 +2,13 @@
 const nextConfig = {
   env:{
     mongo: 'mongodb+srv://tarasfrbslist:gWLUU6cHuXcYCryf@cluster0.f9btobk.mongodb.net/?retryWrites=true&w=majority',
-    siteURL: 'http://localhost:3000'
+    siteURL: 'https://sun-valley.vercel.app'
   }
 }
 
 module.exports = nextConfig
+
+//'https://sun-valley.vercel.app'
+//'http://localhost:3000'
 
 //mongodb+srv://tarasfrbslist:<password>@cluster0.f9btobk.mongodb.net/?retryWrites=true&w=majority
