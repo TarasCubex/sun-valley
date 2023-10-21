d26c1f9717249963dcb88a085d5eac17eff1509f
Taras Stukalo <taras.frbslist@gmail.com>
2023-10-21 13:58:25 +0300

    change url in env

--- next.config.js
+++ next.config.js
@@ -15,7 +15,7 @@ const nextConfig = withPWA({
   },
   env:{
     mongo: 'mongodb+srv://tarasfrbslist:gWLUU6cHuXcYCryf@cluster0.f9btobk.mongodb.net/?retryWrites=true&w=majority',
-    siteURL: 'http://localhost:3000',
+    siteURL: 'https://sun-valley.vercel.app',
   },
 });
 
